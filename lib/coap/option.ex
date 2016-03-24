07976c2a9f969286effbc63c3ba357f59a99d90f
Lucas Torri <lucastorri@gmail.com>
2016-03-24 22:58:50 +0100

    add functions to get flags from option number

--- lib/coap/option.ex
+++ lib/coap/option.ex
@@ -15,4 +15,29 @@ defmodule CoAP.Option do
     %CoAP.Option{number: to_number(name), value: value}
   end
 
+  def critical?(option) do
+    flags(option).critical
+  end
+
+  def unsafe?(option) do
+    flags(option).unsafe
+  end
+
+  def flags(option) do
+    <<
+      _ :: size(3),
+      no_cache_key :: size(3),
+      unsafe :: size(1),
+      critical :: size(1)
+    >> = <<option.number>>
+
+    %{
+      critical: critical != 0,
+      elective: critical == 0,
+      unsafe: unsafe != 0,
+      safe_to_forward: unsafe == 0,
+      no_cache_key: no_cache_key
+    }
+  end
+
 end
